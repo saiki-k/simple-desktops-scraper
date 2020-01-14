e7a2d2f3b53d32534b7cfc2b81f72f6785cd1695
Sai Kishore Komanduri <saikishore.bits@gmail.com>
2020-01-14 11:06:28 +0530

    Fix error-prone parts

--- src/constants.js
+++ src/constants.js
@@ -1,15 +1,16 @@
 const path = require('path');
 
 const BASE_URL = "http://simpledesktops.com";
-const TOTAL_COLLECTION_PAGES = 51;
 
 const DATA_FOLDER = path.join('data');
-const DOWNLOADS_FOLDER = path.join(DATA_FOLDER, 'simple_desktops');
 const META_PATH = path.join(DATA_FOLDER, 'meta.json');
+
+const MAX_COLLECTION_PAGES = 60;
+const DOWNLOADS_FOLDER = path.join(DATA_FOLDER, 'simple_desktops');
 
 module.exports = {
 	BASE_URL,
-	TOTAL_COLLECTION_PAGES,
+	MAX_COLLECTION_PAGES,
 	DATA_FOLDER,
 	DOWNLOADS_FOLDER,
 	META_PATH
