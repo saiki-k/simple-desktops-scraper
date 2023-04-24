63d4cc8a2f1008f32323bed29347110c658feb7a
Sai Kishore Komanduri <saikishore.bits@gmail.com>
2023-04-24 15:56:22 +0530

    Fix the path value for the example DOWNLOADS_FOLDER env. variable

--- env.example.js
+++ env.example.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 process.env = {
-	SIMPLE_DESKTOPS_DOWNLOADS_FOLDER: path.join('./data', 'simple_desktops'),
+	SIMPLE_DESKTOPS_DOWNLOADS_FOLDER: path.join(__dirname, './data', 'simple_desktops'),
 	SIMPLE_DESKTOPS_CHECK_FOR_UPDATES: false,
 	...process.env,
 };
